efcfebfc5cf9fcede8bbc4426b06099ee572a933
Vladislav Nikolov <vladis890@gmail.com>
2019-02-12 21:15:37 +0100

    Added some code. It has to be corrected.

--- src/gif_converter.py
+++ src/gif_converter.py
@@ -1,8 +1,21 @@
-import os
+import os               # os.listdir
+import os.path          # os.path.isfile
 import imageio
 
+supported_formats = ('.jpg', '.png')
+
 class GifConverter:
-    def __init__(in_dir, out_file="out.gif"):
+    def __init__(self, in_dir = '.', out_file = 'out.gif'):
         self.image_list = []
         self.input_dir = input_dir
         self.output_file = out_file
+
+    def read_images(self):
+        # Get all the images that are stored in the directory
+        self.image_list = [imageio.imread(img) for img in os.listdir(self.input_dir) if os.path.isfile(img)
+                           if img.endswith(supported_formats)]
+        # Sort the images so they are in order
+        self.image_list.sort()
+
+    def convert_img_gif(self):
+        imageio.mimsave(self.out_file, self.image_list)
--- src/img2gif.py
+++ src/img2gif.py
@@ -1,7 +1,11 @@
 import gif_converter
 import sys
 
+def parse_arguments(arg_list):
+
 def main(args):
+    arg_list = args.split(' ')
+    parse_arguments(arg_list)
 
 if __name__ == '__main__':
     main(sys.argv)
